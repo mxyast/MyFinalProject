c0ba45e134423de54d59dc0add3de72c5ff0e34d
M.İlyasT <muhammedilyastuman@gmail.com>
2023-11-12 21:11:48 +0300

    Initial commit

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //generic constrain IEntityRepository<T>
+    //where T : class ==>Class referans tip demek
+    //IEntity : IEntity olabilir yada implemente ettiği bir nesne olabilir
+    //new() :newlenebilir olmalı
+    public interface IEntityRepository<T> where T : class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Update(T entity);
+        void Add(T entity);
+        void Delete(T entity);
+
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context: Db tabloları ile proje classlarını bağlamak
+    internal class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true"); 
+            //baştaki @'i  \'ı kullanmak için kullaanıyoruz
+            //optionsBuilder.UseSqlServer(@"Server=175.45.2.12")
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categoies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+
+
+    }
+}
